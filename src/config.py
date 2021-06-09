db9ad39fdcd545ae12a63a20f96f242fe58145d0
jf20541 <jf20541@github.com>
2021-06-09 11:01:44 -0400

    updating readme

--- src/config.py
+++ src/config.py
@@ -1,2 +1,3 @@
 TRAINING_FILE = '../inputs/korea_data.xlsx'
 CLEAN_DATA = '../inputs/clean_data.csv'
+MODEL = '../model/reg_model.sav'
---
+++ src/model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import config
+import statsmodels.api as sm
+import pickle
+
+
+def deploy_model(data):
+    """
+    Parameters: define DV and selected IV's after assumptions
+    return: summary of model 
+    """
+    target = data['GDP_growth']
+    new_features = data[['Broad_money_growth', 'Gov_consumtion_growth','Gross_capital_formation_growth', 'Hh_consumption_growth', 'Pop_growth']]
+    new_features = sm.add_constant(new_features)
+    model = sm.OLS(target,new_features)
+    est = model.fit()
+
+    # save model
+    with open(config.MODEL,'wb') as f:
+        pickle.dump(model, f)
+    return est.summary()
+
+if __name__ == '__main__':
+    df = pd.DataFrame(pd.read_excel(config.TRAINING_FILE))
+    print(deploy_model(df))
